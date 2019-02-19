f07fb65163f9b1602f2ac4f2b4b5bf10ce3f5d7a
Matt Sidwell <msidwell@concurrency.com>
2019-02-19 12:31:22 -0600

    new .md and exercise folders

---
+++ terraform_102/exercise_1/azure_resource_group/azure_resource_group_vars.tf
@@ -0,0 +1,18 @@
+#Variable declarations
+variable "rg_name" {
+  type        = "string"
+  description = "Name for new resource group"
+  default     = "my-fresh-resource-group"
+}
+
+variable "rg_location" {
+  type        = "string"
+  description = "Location for new resource group"
+  default     = "eastus2"
+}
+
+variable "rg_env" {
+  type        = "string"
+  description = "Env tag for new resource group"
+  default     = "eastus2"
+}
